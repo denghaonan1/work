216896aa6352a9521b3f62d931a9e75ab45e13be
32413 <3241347354@qq.com>
2019-08-21 19:59:21 +0800

    l

---
+++ untitled/src/Something.java
@@ -0,0 +1,13 @@
+class Other{
+    public int i;
+}
+class Something {
+    public static void main(String[] args){
+        Other o=new Other();
+        new Something().addOne(o);
+    }
+    public void addOne(final Other o){
+        o.i++;
+       Other o1=new Other();
+    }
+}
